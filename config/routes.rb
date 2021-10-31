ebc9e3b1a6a795378ddb1155a3937ab13d433fd3
helenavocado <hwang92@uchicago.edu>
2021-10-31 01:16:20 +0000

    adskfj

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", {:controller => "directors", :action => "index"})
+
+  get("/directors/eldest", {:controller => "directors", :action => "wisest"})
+
+  get("/directors/:directorid", {:controller => "directors", :action => "director_detail"})
+
 end
